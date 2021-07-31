f68c6c510c40b8e15d5034cb91d9fa5b5cb1bdce
azkarmoulana <azkar.moulana@gmail.com>
2021-07-31 20:33:14 +0530

    handle submit expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,20 +44,47 @@ const ExpenseForm = () => {
     // });
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const newExpense = {
+      title,
+      amount,
+      date: new Date(date),
+    };
+
+    console.log(newExpense);
+    setTitle('');
+    setAmount('');
+    setDate('');
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={title} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={amount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={date}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
