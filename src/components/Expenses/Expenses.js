0a19e1bacdd3e8c40642c80ff42aa1c64ef5c5b6
azkarmoulana <azkar.moulana@gmail.com>
2021-08-07 21:17:02 +0530

    fix filter bug

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,25 +5,29 @@ import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
-  const [items, setItems] = useState(props.items);
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-
-    const filteredItems = props.items.filter(item => {
-      return item.date.getFullYear() === +filteredYear;
-    });
-
-    setItems(filteredItems);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear() === +filteredYear;
+  });
 
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {items.map((expense) => (
-        <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id} />
-      ))}
+      {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+      {filteredExpenses.length > 0 &&
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            key={expense.id}
+          />
+        ))}
     </Card>
   );
 };
